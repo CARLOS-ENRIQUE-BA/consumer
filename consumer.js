1d3c6dfd1f8012300993a2fad56f0b20a33c5f00
carloS <CARLOS-ENRIQUE-BA>
2023-06-29 13:17:45 -0600

    first commit

--- consumer.js
+++ consumer.js
@@ -1,28 +1,47 @@
-const amqp = require('amqplib')
+import { connect as _connect } from 'amqplib';
+import fetch from 'node-fetch';
 
 const rabbitSettings = {
     protocol: 'amqp',
-    hostname: 'localhost',
+    hostname: '54.158.119.212',
     port: 5672,
-    username: 'guest',
-    password: 'guest'
-}
+    username: 'CarlosAguilar',
+    password: 'KDF123'
+};
+
 async function connect() {
-    const queue = "estafeta"
+    const queue = 'InitialEvent';
     try {
-        const conn = await amqp.connect(rabbitSettings);
-        console.log('Conexión exitosa')
-        const channel = await conn.createChannel()
-        console.log ("Canal creado exitosamente")
+        const conn = await _connect(rabbitSettings);
+        console.log('Conexión exitosa');
+        const channel = await conn.createChannel();
+        console.log('Canal creado exitosamente');
 
-        channel.consume(queue, (msn)=> {
-            console.log(msn.content.toString())
-            channel.ack(msn)
-        })
-        
+    channel.consume(queue, async (msn) => {
+        const messageContent = msn.content.toString();
+        console.log(messageContent);
+        try {
+            const response = await fetch('http://52.21.3.207:3003/products/', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json"
+            },
+            body: messageContent
+        });
 
+        if (response.ok) {
+            console.log('Mensaje enviado a la API');
+        } else {
+            console.error('Error al enviar mensaje');
+        }
+        } catch (error) {
+            console.error('Error al llamar la API', error);
+        }
+        channel.ack(msn);
+    });
     } catch (error) {
-        console.error('Error => ', error)    
+        console.error('Error:', error);
     }
 }
 
